1b0991cf7bb8b390efa846dcb4d68a4d9ed24e24
Ariel Wagowsky <arielw@sqreamtech.com>
2024-12-11 17:06:55 +0200

    minor update to setp.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
-from pysqream_blue.globals import __version__
 
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as req:
-    requires = req.read().split('\n')[:-1]
+    requires = req.read().splitlines()
 
 setup_params = dict(
-    name =          'pysqream-blue',
-    version =       __version__,
-    description =   'DB-API connector for SQream DB', 
-    long_description_content_type = long_description,
-    url = "https://github.com/SQream/pysqream-blue",
-    author = "SQream",
-    author_email = "info@sqream.com",
-    packages = ["pysqream_blue", "protos"],
-    classifiers = [
+    name='pysqream-blue',
+    version='1.0.0',  # Directly specify the version here
+    description='DB-API connector for SQream DB',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/SQream/pysqream-blue",
+    author="SQream",
+    author_email="info@sqream.com",
+    packages=find_packages(),
+    classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    keywords = 'database db-api sqream sqreamdbV2',
-    python_requires = '>=3.9',
+    keywords='database db-api sqream sqreamdbV2',
+    python_requires='>=3.9',
     install_requires=requires
 )
 

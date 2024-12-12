012d2bc69d77918f2b2916ad7e61b2902f28d3b6
Ariel Wagowsky <arielw@sqreamtech.com>
2024-12-12 12:57:25 +0200

    Add acccess_token as a flag

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 
 def pytest_addoption(parser):
     parser.addoption("--domain", action="store", help="SQream Server ip", required=True)
+    parser.addoption("--access_token", action="store", help="SQream Access token", required=True)
     parser.addoption("--use_logs", action="store_true", help="use logs", default=False)
     parser.addoption("--log_path", action="store", help="path of logs", default=None)
     parser.addoption("--log_level", action="store", help="log level", default='INFO')
@@ -8,3 +9,4 @@ def pytest_addoption(parser):
 
 def pytest_generate_tests(metafunc):
     metafunc.config.getoption("domain")
+    metafunc.config.getoption("access_token")

3606c3031a0f022cba33799382bef8db86e0673c
Ariel Wagowsky <arielw@sqreamtech.com>
2024-12-12 14:28:54 +0200

    Add new flag

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 
 def pytest_addoption(parser):
-    parser.addoption("--domain", action="store", help="SQream Server ip", required=True)
-    parser.addoption("--access_token", action="store", help="SQream Access token", required=True)
+    parser.addoption("--domain", action="store", help="Blue Domain ip", required=True)
+    parser.addoption("--access_token", action="store", help="Blue Access token", required=True)
     parser.addoption("--use_logs", action="store_true", help="use logs", default=False)
     parser.addoption("--log_path", action="store", help="path of logs", default=None)
     parser.addoption("--log_level", action="store", help="log level", default='INFO')
